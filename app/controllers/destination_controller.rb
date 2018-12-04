f8075e14db048b6ff29bbeaa443833b6007049d7
Filippo <filippo.luppi@gmail.com>
2018-12-04 23:53:17 +0000

    all works

--- app/controllers/destination_controller.rb
+++ app/controllers/destination_controller.rb
@@ -3,7 +3,7 @@ class DestinationController < ApplicationController
     
     def index
         
-      sql = "SELECT prodottos.id, prodottos.nome, riga_comandas.modifiche, comandas.tavolo,riga_comandas.consegnata, cat_prodottos.id_destination,riga_comandas.quantit, riga_comandas.id FROM cat_prodottos, prodottos,destinaziones,comandas,riga_comandas WHERE prodottos.id_cat=cat_prodottos.id AND cat_prodottos.id_destination=destinaziones.id AND prodottos.id=riga_comandas.id_prodotto AND riga_comandas.id_comanda=comandas.id And riga_comandas.consegnata=0"
+      sql = "SELECT prodottos.id, prodottos.nome, riga_comandas.modifiche, comandas.tavolo,riga_comandas.consegnata, cat_prodottos.id_destination,riga_comandas.quantit, riga_comandas.id FROM cat_prodottos, prodottos,destinaziones,comandas,riga_comandas WHERE prodottos.id_cat=cat_prodottos.id AND cat_prodottos.id_destination=destinaziones.id AND prodottos.id=riga_comandas.id_prodotto AND riga_comandas.id_comanda=comandas.id And riga_comandas.consegnata!=1"
     records_array = ActiveRecord::Base.connection.execute(sql)
 
     respond_to do |format|
